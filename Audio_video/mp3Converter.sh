411e4d605a080e538357a6e1e1e9ed24fb2a11d9
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2015-06-23 19:09:34 +0200

    Update mp3Converter.sh

--- Audio_video/mp3Converter.sh
+++ Audio_video/mp3Converter.sh
@@ -5,5 +5,3 @@ for f in *.wav;do lame -V 1 $f ${f%.wav}.mp3; done
 rm *.wav
 cd ..
 #Nell'ordine: sostituisce gli spazi bianchi con _; converte in mp3; rimuove i .wav
-##Merge pdf
-# pdftk 1.pdf 2.pdf 3.pdf cat output 123.pdf
