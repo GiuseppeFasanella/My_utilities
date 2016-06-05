055224d9154a6cc9c658f7b474c21ca356ecfec4
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2016-06-05 13:19:47 +0200

    Update mp3Converter.sh

--- Audio_video/mp3Converter.sh
+++ Audio_video/mp3Converter.sh
@@ -6,3 +6,6 @@ rm *.wav
 cd ..
 #Nell'ordine: sostituisce gli spazi bianchi con _; converte in mp3; rimuove i .wav
 #NON FUNZIONA CON i .ram e gli inutili .wma (windows media audio)
+Nel caso generale usa
+
+https://github.com/GiuseppeFasanella/My_utilities/blob/master/Audio_video/Convert_any_file_to_mp3.sh
