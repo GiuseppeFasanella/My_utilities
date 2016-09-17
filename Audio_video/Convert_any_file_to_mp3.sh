616641f3b30958f8a37e2d3238da729ad289dbcb
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2016-09-17 13:23:43 +0200

    Update Convert_any_file_to_mp3.sh

--- Audio_video/Convert_any_file_to_mp3.sh
+++ Audio_video/Convert_any_file_to_mp3.sh
@@ -3,5 +3,5 @@ mplayer -playlist perchisuonalacampana20.ram -ao pcm:file=stream.wav
 #A questo punto, essendo passati da ram a wav, puoi passare da was a mp3
 mv stream.wav file.wav
 f=file.wav
-lame -V 1 $f ${f%.wav}.mp3; done
-rm *.wav
+lame -V 1 $f ${f%.wav}.mp3
+rm $f
