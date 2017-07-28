83092bf5d687fad46049250e7c82c45d5105acb2
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2017-07-28 18:23:01 +0200

    Update cut_musichetta_ad_alta_voce.sh

--- Audio_video/cut_musichetta_ad_alta_voce.sh
+++ Audio_video/cut_musichetta_ad_alta_voce.sh
@@ -1,4 +1,11 @@
 for f in *.mp3; do
-    ffmpeg -i $f -ss 00:21  -c copy ${f%.mp3}_reduced.mp3
-    #La musichetta orribile finisce al 21-mo secondo                                                                                                                            
+    #La musichetta iniziale orribile finisce al 21-mo secondo
+    #La musichetta finale orribile inizia a 31 secondi dalla fine del brano
+
+    #echo "`ffprobe -v 0 -show_entries format=duration -of compact=p=0:nk=1 $f`" #-->total length
+    #echo "`ffprobe -v 0 -show_entries format=duration -of compact=p=0:nk=1 $f` -31" | bc --> total length meno 31 secondi
+    reduced_length=$(echo "`ffprobe -v 0 -show_entries format=duration -of compact=p=0:nk=1 $f` -31" | bc)
+    #echo $reduced_length
+    ffmpeg -i $f -ss 00:21 -to $reduced_length -c copy ${f%.mp3}_reduced.mp3 
+
 done
