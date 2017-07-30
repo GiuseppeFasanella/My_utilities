7093bfaeacca00b24de66eec5401133877ea4562
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2017-07-30 23:21:19 +0200

    Create dplay_download.sh

---
+++ Audio_video/dplay_download.sh
@@ -0,0 +1,3 @@
+ffmpeg -i "https://dplayit.akamaized.net/EHD_945779B_DPLAY_ITALY/EHD_945779B_DPLAY_ITALY-master.m3u8?hdnts=st=1495371544~exp=1495371844~acl=/*~hmac=f05e8e6a529452ae54f5289623ceb8b79b2c8ad32a90dd4edf6c31f2afeb048f" -c copy -map 0:0 -map 0:1 "Unti_e_Bisunti_-_S2_E11_-_stagione_2_episodio_11_(480x270).mp4"
+
+http://danielegiudice.altervista.org/scaricare-i-video-di-dplay-it/
