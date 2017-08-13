1cf9b715fdaef12d86bf51fdb2f337e418b817e8
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2017-08-13 14:23:51 +0200

    Update Remove_contour.sh

--- Pdf_Png_string/Remove_contour.sh
+++ Pdf_Png_string/Remove_contour.sh
@@ -8,3 +8,30 @@ done
 #Lavora solo sul nome e taglia l'estensione.                                                                                                                                    
 #Fai una copia di sicurezza.                                                                                                                                                    
 #Taglia il contorno con convert. La sintassi e' widthxheight+x+y (che prendi da gimp, provando a selezionare la parte che ti interessa)
+
+################### More flexible structure
+f=/home/gfasanel/Immagini/test/probe_ptDataMc_icut1_
+#.png                                                                                                                                                                           
+#pixel_dim=$(identify $1 | awk -F" " '{print $3}')                                                                                                                              
+#ls $f                                                                                                                                                                          
+
+pixel_dim=$(identify $f.png | awk -F" " '{print $3}')
+x_size=$(echo $pixel_dim | awk -F"x" '{print $1}')
+y_size=$(echo $pixel_dim | awk -F"x" '{print $2}')
+
+echo $pixel_dim
+width=$2
+height=$3
+x=$4
+y=$5
+
+width=`echo $width*$x_size | bc -l`
+height=`echo $height*$y_size | bc -l`
+x_size=`echo $x*$x_size | bc -l`
+y_size=`echo $y*$y_size | bc -l`
+echo $width
+echo $height
+echo $x_size
+echo $y_size
+#cp ${f}.png ${f}_full.png;                                                                                                                                                     
+convert ${f}.png[${width}x${height}+${x_size}+${y_size}] ${f}_test.png
