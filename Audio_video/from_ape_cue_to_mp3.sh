d2ab1cc33d29e6e4346239be40c1ef638437755c
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2017-10-14 15:25:07 +0200

    Update from_ape_cue_to_mp3.sh

--- Audio_video/from_ape_cue_to_mp3.sh
+++ Audio_video/from_ape_cue_to_mp3.sh
@@ -10,15 +10,16 @@ avconv -i $1.ape $1.mp3
 
 # Now, split the MP3 file using the CUE file
 # this will produce an mp3 file for each track in the same directory
-mp3 split -a -c $1.cue $1.mp3
+mp3splt -a -c $1.cue $1.mp3
 
+#If you  prefer APE format uncomment this
 # Next example: FLAC!
 # convert APE to FLAC:
-ffmpeg -i $1.ape $1.flac
+##ffmpeg -i $1.ape $1.flac
 
 # Now, split your FLAC file. Credits for method go to the arch linux wiki:
 # https://wiki.archlinux.org/index.php/CUE_Splitting
-shnsplit -f $1.cue -t "%n %t" -o flac $1.flac 
+##shnsplit -f $1.cue -t "%n %t" -o flac $1.flac 
 
 # one more note, you can go directly from APE -> FLAC with shnsplit because that
 # tool is fucking awesome, but you need the "mac" encoder. This is a pain in the ass to get
