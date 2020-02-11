70ae5a30b32699d2c92b08eaec816941ab3cdd71
GiuseppeFasanella <giuseppe.fasanella2@gmail.com>
2020-02-11 15:57:05 +0100

    Update From_epub_to_pdf.sh

--- Pdf_Png_string/From_epub_to_pdf.sh
+++ Pdf_Png_string/From_epub_to_pdf.sh
@@ -1,4 +1,7 @@
 #1. Apri calibre e converti da epub a pdf
 # Ci sara' un problema di margini: il pdf convertito non li possiede: occorre darglieli
+#pdfjam --scale '0.8' libro.pdf
 
-pdfjam --scale '0.8' libro.pdf
+## 2. Versione molto piu' comoda. Chiama da riga di comando calibre direttamente.
+ebook-convert Doctor_Fauses.epub Doctor_Faustus.pdf
+#pdfjam --scale '0.8' Doctor_Faustus.pdf ##Scala se vuoi lasciarti spazio per le note
